7664e0be762f39a0126c5bcd540b13c603eee225
crefr <fedor30122005@gmail.com>
2024-08-20 18:43:16 +0300

    structures
    now all coefficients and roots are in structures, functions receive structures as arguments

--- main.cpp
+++ main.cpp
@@ -6,103 +6,107 @@
 const int INF_ROOTS = -1;
 const double EPSILON = 0.00001;
 
-int sqSolve(const double a, const double b, const double c, double *x1, double *x2);
+void sqSolve(struct coeffs_t coef, struct roots_t *root);
 int isZero(const double a);
-void scanCoefs(double *a, double *b, double *c);
-void printRoots(const int nRoots, const double x1, const double x2);
+void scanCoefs(struct coeffs_t *coeffs);
+void printRoots(struct roots_t root);
+double discr(struct coeffs_t coef);
+int linSolve(double b, double c, double *x);
+
+struct coeffs_t
+{
+    double a = 0;
+    double b = 0;
+    double c = 0;
+};
+
+struct roots_t
+{
+    double x1 = 0;
+    double x2 = 0;
+    int nRoots = -2;
+};
 
 int main()
 {
+    struct coeffs_t sq;
+    struct roots_t root;
+
     printf("# SquareSolver\n"
            "# Made for Summer school MIPT\n");
 
     printf("Enter a, b, c coefficients:\n");
 
-    double a = 0, b = 0, c = 0;
-    scanCoefs(&a, &b, &c);
+    scanCoefs(&sq);
 
-    double x1 = 0, x2 = 0;
-    const int nRoots = sqSolve(a, b, c, &x1, &x2);
-    printRoots(nRoots, x1, x2);
+    sqSolve(sq, &root);
+    printRoots(root);
 
     return 0;
 }
 
-
-//! @brief sqSolve - function that solves square equations and returns real roots
-//!
-//! @param   [in]     a     coefficient a
-//! @param   [in]     b     coefficient b
-//! @param   [in]     c     coefficient c
-//! @param   [out]    x1    pointer to first root
-//! @param   [out]    x2    pointer to second root
-//!
-//! @return number of real roots for the equation
-//! @note In case of infinite roots returns INF_ROOTS
-
-int sqSolve(const double a, const double b, const double c, double *x1, double *x2)
+void sqSolve(struct coeffs_t coef, struct roots_t *root)
 {
-    assert(isfinite(a));
-    assert(isfinite(b));
-    assert(isfinite(c));
+    assert(root != NULL);
 
-    assert(x1 != NULL);
-    assert(x2 != NULL);
-    assert(x1 != x2);
+    assert(isfinite(coef.a));
+    assert(isfinite(coef.b));
+    assert(isfinite(coef.c));
 
-
-    if (isZero(a))
+    if (isZero(coef.a))
     {
-        if (isZero(b))
-            return (isZero(c)) ? INF_ROOTS : 0;
-        *x1 = -c/b;
-        return 1;
+        root -> nRoots = linSolve(coef.b, coef.c, &(root -> x1));
+        return;
     }
 
-    double D = b*b - 4*a*c;
+    double D = discr(coef);
     if (isZero(D))
     {
-        *x1 = -b/(2*a);
-        return 1;
+        root -> x1 = -coef.b/(2*coef.a);
+        root -> nRoots =  1;
+        return;
     }
     if (D > 0)
     {
         double sqrt_D = sqrt(D);
-        *x1 = (-b+sqrt_D)/(2*a);
-        *x2 = (-b-sqrt_D)/(2*a);
-        return 2;
+        root -> x1 = (-coef.b+sqrt_D)/(2*coef.a);
+        root -> x2 = (-coef.b-sqrt_D)/(2*coef.a);
+        root -> nRoots = 2;
+        return;
     }
 
-    return 0;
+    root -> nRoots = 0;
 }
 
 int isZero(const double a)
 {
+    assert(isfinite(a));
     return fabs(a) < EPSILON;
 }
 
-void scanCoefs(double *a, double *b, double *c)
+void scanCoefs(struct coeffs_t *sq)
 {
-    while(scanf("%lg %lg %lg", a, b, c) != 3)  {
-        int ch;
+    assert(sq != NULL);
+    while(scanf("%lg %lg %lg", &(sq -> a), &(sq -> b), &(sq -> c)) != 3)  {
+        int ch = EOF;
         while ((ch = getchar()) != '\n' && ch != EOF);
         printf("Try again\n");
     }
 }
 
-void printRoots(const int nRoots, const double x1, const double x2){
-    switch(nRoots)
+void printRoots(struct roots_t root){
+    switch(root.nRoots)
     {
         case 0:
             printf("No real roots");
             break;
 
         case 1:
-            printf("x = %lg\n", x1);
+            printf("x = %lg\n", root.x1);
             break;
 
         case 2:
-            printf("x1 = %lg\nx2 = %lg\n", x1, x2);
+            printf("x1 = %lg\nx2 = %lg\n", root.x1, root.x2);
             break;
 
         case INF_ROOTS:
@@ -110,9 +114,32 @@ void printRoots(const int nRoots, const double x1, const double x2){
             break;
 
         default:
-            printf("ERROR nRoots = %d\n", nRoots);
+            printf("ERROR nRoots = %d\n", root.nRoots);
             break;
     }
 }
 
+int linSolve(double b, double c, double *x)
+{
+    assert(x != NULL);
+    assert(isfinite(b));
+    assert(isfinite(c));
 
+    if (isZero(b))
+    {
+        if (isZero(c))
+            return INF_ROOTS;
+        return 0;
+    }
+    *x = -c/b;
+    return 1;
+}
+
+double discr(struct coeffs_t coef)
+{
+    assert(isfinite(coef.a));
+    assert(isfinite(coef.b));
+    assert(isfinite(coef.c));
+
+    return coef.b*coef.b - 4*coef.a*coef.c;
+}
