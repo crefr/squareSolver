5e0f5d52f3cd9fffdfb96c273c6d39f8be7e59de
crefr <fedor30122005@gmail.com>
2024-08-22 18:25:01 +0300

    fixed cmd args, made colPrintf

--- main.cpp
+++ main.cpp
@@ -7,8 +7,25 @@
 #include <string.h>
 //#include "TXLib.h"
 #include <ctype.h>
+#include <stdarg.h>
 
 #include "colors.h"
+
+const int INF_ROOTS = -1;
+const double EPSILON = 0.00001;
+
+const int ERROR_ = -10;
+enum EXITS {BAD, GOOD};
+
+const int MAXLINE = 100;
+
+struct argvEx_t
+{
+    unsigned int haveVal : 1;
+    char fname[MAXLINE];
+    unsigned int fval : 1;
+
+};
 
 //! @brief  Структура, содержащая 3 коэффициента квадратного уравнения (a, b, c)
 struct coeffs_t
@@ -42,11 +59,15 @@ struct flags_t
 
 struct flags_t modes = {0, 0, 1};
 
-const int INF_ROOTS = -1;
-const double EPSILON = 0.00001;
-
-const int ERROR_ = -10;
-enum EXITS {BAD, GOOD};
+enum flags {H = 0, HELP, M, C};
+struct argvEx_t args[] =
+{
+    {.haveVal = 0, .fname = "-h", .fval = 0},
+    {.haveVal = 0, .fname = "--help", .fval = 0},
+    {.haveVal = 1, .fname = "-m", .fval = 0},
+    {.haveVal = 1, .fname = "-c", .fval = 1},
+};
+const int argvnum = sizeof(args) / sizeof(argvEx_t);
 
 void sqSolve(struct coeffs_t coef, struct roots_t *root);
 int isZero(const double a);
@@ -59,6 +80,7 @@ int equals(double a, double b);
 int runTests(struct test_t tests[], int len);
 bool eqRoots(roots_t root1, roots_t root2);
 int argvReceive(int argc, char **argv);
+void colPrintf(const char *color, const char *format, ...);
 
 #include "tests.h"
 
@@ -71,11 +93,11 @@ int main(int argc, char *argv[])
 {
     if (argvReceive(argc, argv) == BAD)
     {
-        printf("CMD args ERROR\n");
+        colPrintf(RED, "CMD args ERROR\n");
         return ERROR_;
     }
 
-    if (modes.h == 1)
+    if (args[H].fval == 1 || args[HELP].fval == 1)
     {
         printf("#######################################################\n");
         printf("-m        \tswitches mode to 0 or 1 (-m 0 and -m 1)\n"
@@ -85,11 +107,11 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    if (modes.m == 1)
+    if (args[M].fval == 1)
     {
         if (!runTests(tests, TESTNUM))
         {
-            printf("TESTING FAILED\n");
+            colPrintf(RED, "TESTING FAILED\n");
             return ERROR_;
         }
         return 0;
@@ -104,7 +126,8 @@ int main(int argc, char *argv[])
 
     if (scanCoefs(&sq) == 0)
     {
-        printf((modes.c) ? RED "!!!INPUT ERROR!!!" : "!!!INPUT ERROR!!!");
+        colPrintf(RED, "!!!INPUT ERROR!!!\n");
+        //printf((args[C].fval) ? RED "!!!INPUT ERROR!!!" : "!!!INPUT ERROR!!!");
         return ERROR_;
     }
 
@@ -233,23 +256,28 @@ void printRoots(struct roots_t root){
     switch(root.nRoots)
     {
         case 0:
-            printf((modes.c) ? RED "No real roots" RESET_C : "No real roots");
+            colPrintf(RED, "No real roots");
+            //printf((args[C].fval) ? RED "No real roots" RESET_C : "No real roots");
             break;
 
         case 1:
-            printf((modes.c) ? GREEN "x = %lg\n" RESET_C : "x = %lg\n", root.x1);
+            colPrintf(GREEN, "x = %lg\n", root.x1);
+            //printf((args[C].fval) ? GREEN "x = %lg\n" RESET_C : "x = %lg\n", root.x1);
             break;
 
         case 2:
-            printf((modes.c) ? GREEN "x1 = %lg\nx2 = %lg\n" RESET_C : "x1 = %lg\nx2 = %lg\n", root.x1, root.x2);
+            colPrintf(GREEN, "x1 = %lg\nx2 = %lg\n", root.x1, root.x2);
+            //printf((args[C].fval) ? GREEN "x1 = %lg\nx2 = %lg\n" RESET_C : "x1 = %lg\nx2 = %lg\n", root.x1, root.x2);
             break;
 
         case INF_ROOTS:
-            printf((modes.c) ? GREEN "Infinite roots\n" RESET_C : "Infinite roots\n");
+            colPrintf(GREEN, "Infinite roots\n");
+            //printf((args[C].fval) ? GREEN "Infinite roots\n" RESET_C : "Infinite roots\n");
             break;
 
         default:
-            printf((modes.c) ? GREEN "ERROR nRoots = %d\n" RESET_C : "Infinite roots\n" , root.nRoots);
+            colPrintf(GREEN, "ERROR nRoots = %d\n", root.nRoots);
+            //printf((args[C].fval) ? GREEN "ERROR nRoots = %d\n" RESET_C : "Infinite roots\n" , root.nRoots);
             break;
     }
 }
@@ -308,14 +336,20 @@ int runTest(struct test_t test)
         #ifdef _TX_MODULE
             txSetConsoleAttr(4) meow
         #endif
-        printf((modes.c) ? RED "Error Test %d:\na = %lg, b = %lg, c = %lg\n"
-               "x1 = %lg, x2 = %lg, nRoots = %d\n"
-               "Expected:\nx1 = %lg, x2 = %lg, nRoots = %d\n" RESET_C
-               : "Error Test %d:\na = %lg, b = %lg, c = %lg\n"
-               "x1 = %lg, x2 = %lg, nRoots = %d\n"
-               "Expected:\nx1 = %lg, x2 = %lg, nRoots = %d\n",
-               test.num, test.coef.a, test.coef.b, test.coef.c, root.x1, root.x2, root.nRoots,
-               test.root.x1, test.root.x2, test.root.nRoots);
+
+        colPrintf(RED, "Error Test %d:\na = %lg, b = %lg, c = %lg\n"
+                       "x1 = %lg, x2 = %lg, nRoots = %d\n"
+                       "Expected:\nx1 = %lg, x2 = %lg, nRoots = %d\n",
+                        test.num, test.coef.a, test.coef.b, test.coef.c, root.x1, root.x2, root.nRoots,
+                        test.root.x1, test.root.x2, test.root.nRoots);
+        // printf((args[C].fval) ? RED "Error Test %d:\na = %lg, b = %lg, c = %lg\n"
+        //        "x1 = %lg, x2 = %lg, nRoots = %d\n"
+        //        "Expected:\nx1 = %lg, x2 = %lg, nRoots = %d\n" RESET_C
+        //        : "Error Test %d:\na = %lg, b = %lg, c = %lg\n"
+        //        "x1 = %lg, x2 = %lg, nRoots = %d\n"
+        //        "Expected:\nx1 = %lg, x2 = %lg, nRoots = %d\n",
+        //        test.num, test.coef.a, test.coef.b, test.coef.c, root.x1, root.x2, root.nRoots,
+        //        test.root.x1, test.root.x2, test.root.nRoots);
         #ifdef _TX_MODULE
             txSetConsoleAttr(7) meow
         #endif
@@ -324,7 +358,9 @@ int runTest(struct test_t test)
     #ifdef _TX_MODULE
         txSetConsoleAttr(2) meow
     #endif
-    printf((modes.c) ? GREEN "Test %d Correct\n" RESET_C : "Test %d Correct\n", test.num);
+
+    colPrintf(GREEN, "Test %d Correct\n", test.num);
+    //printf((args[C].fval) ? GREEN "Test %d Correct\n" RESET_C : "Test %d Correct\n", test.num);
 
     #ifdef _TX_MODULE
         txSetConsoleAttr(7) meow
@@ -351,57 +387,83 @@ bool eqRoots(roots_t root1, roots_t root2)
         return 1;
     return 0;
 }
+
 int argvReceive(int argc, char **argv)
 {
-    assert(argv!=NULL);
-
-    enum conf_state {READY, VAL};
-    int mconf = VAL;
-    int cconf = VAL;
-
     for(int i = 1; i < argc; i++)
     {
-        if (argc == 2 && (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0))
+        int j;
+        for (j = 0; j < argvnum; j++)
         {
-            modes.h = 1;
-            return GOOD;
-        }
-        else if (strcmp("-m", argv[i]) == 0 && mconf == VAL && cconf == VAL)
-            mconf = READY;
-        else if (strcmp("-c", argv[i]) == 0 && mconf == VAL && cconf == VAL)
-            cconf = READY;
-
-        else if (mconf == READY)
-        {
-            mconf = VAL;
-            if (strcmp("0", argv[i]) == 0)
-                modes.m = 0;
-            else if (strcmp("1", argv[i]) == 0)
-                modes.m = 1;
-            else
-                return BAD;
-        }
-
-        else if (cconf == READY)
-        {
-            cconf = VAL;
-            if (strcmp("0", argv[i]) == 0)
-                modes.c = 0;
-            else if (strcmp("1", argv[i]) == 0)
-                modes.c = 1;
-            else
-                return BAD;
-        }
-        else
-            return BAD;
+            if (strcmp(argv[i], args[j].fname) == 0)
+            {
+                if (args[j].haveVal == 0)
+                    args[j].fval = 1;
+                else
+                {
+                    i++;
+                    if (i >= argc)
+                        return BAD;
+                    if (strcmp(argv[i], "0") == 0)
+                        args[j].fval = 0;
+                    else if (strcmp(argv[i], "1") == 0)
+                        args[j].fval = 1;
+                    else
+                        return BAD;
+                }
+                break;
+            }
+        } if(j == argvnum) return BAD;
     }
-
-    if (mconf == READY || cconf == READY)
-        return BAD;
     return GOOD;
 }
 
+void colPrintf(const char *color, const char *format, ...)
+{
+    if (args[C].fval == 1)
+        printf("%s", color);
+    va_list ap;
+    va_start(ap, format);
+
+    const char *p;
+    int lflag = 0;
+    for (p = format; *p != '\0'; p++)
+    {
+        if(*p != '%' && !lflag)
+        {
+            putchar(*p);
+            continue;
+        }
+
+        if (lflag)
+            lflag = 0;
+        else
+            p++;
+
+        switch(*p)
+        {
+            case 'l':
+                lflag = 1;
+                break;
+            case 'd':
+                printf("%d", va_arg(ap, int));
+                break;
+            case 'f': case 'g':
+                printf("%lg", va_arg(ap, double));
+                break;
+            case 's':
+                printf("%s", va_arg(ap, char*));
+                break;
+            default:
+                putchar(*p);
+                break;
+        }
+    }
+    va_end(ap);
+    printf(RESET_C);
+}
+
 /*
-структуры для командной строки
-цветной printf
+структуры для командной строки +
+цветной printf +
 */
